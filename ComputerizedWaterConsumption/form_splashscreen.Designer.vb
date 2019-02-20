fc2cc1a3f955fc16ec36270fb9706540008c665a
Joshua Arosco <joshuaarosco.hs@gmail.com>
2019-02-20 21:23:26 +0800

    Update

---
+++ ComputerizedWaterConsumption/form_splashscreen.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_splashscreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_splashscreen))
+        Me.pb_loading = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_copyright = New System.Windows.Forms.Label()
+        Me.lbl_version = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lbl_loading = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pb_loading
+        '
+        Me.pb_loading.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.pb_loading.ForeColor = System.Drawing.Color.DodgerBlue
+        Me.pb_loading.Location = New System.Drawing.Point(31, 253)
+        Me.pb_loading.Minimum = 1
+        Me.pb_loading.Name = "pb_loading"
+        Me.pb_loading.Size = New System.Drawing.Size(219, 3)
+        Me.pb_loading.TabIndex = 2
+        Me.pb_loading.Value = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(30, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(224, 58)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Computerized" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Water Consumption"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_copyright
+        '
+        Me.lbl_copyright.AutoSize = True
+        Me.lbl_copyright.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_copyright.Location = New System.Drawing.Point(28, 305)
+        Me.lbl_copyright.Name = "lbl_copyright"
+        Me.lbl_copyright.Size = New System.Drawing.Size(89, 15)
+        Me.lbl_copyright.TabIndex = 5
+        Me.lbl_copyright.Text = "Copyright 2019"
+        '
+        'lbl_version
+        '
+        Me.lbl_version.AutoSize = True
+        Me.lbl_version.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_version.Location = New System.Drawing.Point(28, 290)
+        Me.lbl_version.Name = "lbl_version"
+        Me.lbl_version.Size = New System.Drawing.Size(68, 15)
+        Me.lbl_version.TabIndex = 4
+        Me.lbl_version.Text = "Version 1.0"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.lbl_loading)
+        Me.Panel1.Controls.Add(Me.lbl_copyright)
+        Me.Panel1.Controls.Add(Me.lbl_version)
+        Me.Panel1.Controls.Add(Me.pb_loading)
+        Me.Panel1.Location = New System.Drawing.Point(267, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(283, 350)
+        Me.Panel1.TabIndex = 8
+        '
+        'lbl_loading
+        '
+        Me.lbl_loading.AutoSize = True
+        Me.lbl_loading.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_loading.Location = New System.Drawing.Point(28, 235)
+        Me.lbl_loading.Name = "lbl_loading"
+        Me.lbl_loading.Size = New System.Drawing.Size(64, 15)
+        Me.lbl_loading.TabIndex = 6
+        Me.lbl_loading.Text = "Loading ..."
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.ComputerizedWaterConsumption.My.Resources.Resources.bg1
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(275, 350)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox2.BackgroundImage = Global.ComputerizedWaterConsumption.My.Resources.Resources.water
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(97, 34)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(86, 86)
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'frm_splashscreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(550, 350)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frm_splashscreen"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pb_loading As System.Windows.Forms.ProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lbl_copyright As System.Windows.Forms.Label
+    Friend WithEvents lbl_version As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lbl_loading As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+End Class
---
+++ ComputerizedWaterConsumption/form_splashscreen.vb
@@ -0,0 +1,24 @@
+﻿Public Class frm_splashscreen
+
+    Private Sub frm_splashscreen_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+
+        lbl_version.Text = "Version 1.0"
+
+        'Copyright info
+        lbl_copyright.Text = My.Application.Info.Copyright + ", IskolarCreatives"
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If pb_loading.Value <= pb_loading.Maximum - 1 Then
+            pb_loading.Value += 1
+        Else
+            Timer1.Enabled = False
+            Dim frm = New frm_login()
+
+            frm.Show()
+            Me.Hide()
+        End If
+    End Sub
+
+End Class
